98c1dc28466ba1c89f77f919e5d0ec5929863a3a
Your Name <you@example.com>
2017-02-03 15:00:20 -0500

    still working on getting some information from Spotify and I am styling everything and then putting them into the partial folders.

--- script/content.js
+++ script/content.js
@@ -5,7 +5,7 @@ var resultItems = document.querySelector('.results');
 submit.addEventListener('click', function(e){
 	e.preventDefault();
 	var query = search.value;
-	var limit = "15";
+	var limit = "9";
 	var type = "album";
 	var api = `https://api.spotify.com/v1/search?query=${query}&offset=0&limit=${limit}&type=${type}`;
 	console.log(api);
@@ -21,8 +21,6 @@ function makeRequest(url){
 		if(request.status >= 200 && request.status < 400){
 			var data = JSON.parse(request.responseText);
 			renderPage(data.albums.items);
-			
-
 		}
 		else{
 			console.log('response error', request);
@@ -32,25 +30,33 @@ function makeRequest(url){
 		console.log('connection error')
 	}
 	request.send();
-	console.log("anything")
+	// console.log("anything")
 }
-function renderPage(results){
+function renderPage(results, re){
 
-		var listItem = "";
-		console.log(results)
+	var listItem = "";
+		console.log(re)
 
 		for (var i = 0; i < results.length; i++) {
+			
+
 			listItem += '<article>';
-			listItem += '<img src="'+ results[i].images +'" alt="" />';
-			listItem += '<h2>' + results[i].name + '</h2>';
+			listItem += '<img src="'+ results[i].artists.images +'" alt="" />';
+			listItem += '<h3>' + results[i].name + '</h3>';
 			listItem += '<p>' + results[i].artists.name + '</p>';
 			listItem += '</article>';
+				
 		}
 
 		resultItems.insertAdjacentHTML('beforeend', listItem);
 	
 }
+
+		
+	
 // results.forEach((result, index, array) => {
-	// 	var listItem = document.createElement('li');
-	// 	listItem.innerHTML = result.name;
-	// 	resultItems.appendChild(listItem);
+// 		var listItem = document.createElement('li');
+// 		listItem.innerHTML = result.name;
+// 		resultItems.appendChild(listItem);
+
+	// })
