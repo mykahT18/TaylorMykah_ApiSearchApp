b8d3b6ae7c559113ede036a55c6e9c14f0529942
Your Name <you@example.com>
2017-02-03 01:29:01 -0500

    I have started styling it. I am writing the css in plain first then I am going to transfer it to scss.

--- script/content.js
+++ script/content.js
@@ -5,7 +5,7 @@ var resultItems = document.querySelector('.results');
 submit.addEventListener('click', function(e){
 	e.preventDefault();
 	var query = search.value;
-	var limit = "10";
+	var limit = "15";
 	var type = "album";
 	var api = `https://api.spotify.com/v1/search?query=${query}&offset=0&limit=${limit}&type=${type}`;
 	console.log(api);
@@ -13,14 +13,16 @@ submit.addEventListener('click', function(e){
 })
 function makeRequest(url){
 	var request = new XMLHttpRequest();
-	
+
 	request.open('GET', url, true)
 
 	request.onload = function(){
-		console.log("anything")
+		
 		if(request.status >= 200 && request.status < 400){
 			var data = JSON.parse(request.responseText);
-			console.log(data);
+			renderPage(data.albums.items);
+			
+
 		}
 		else{
 			console.log('response error', request);
@@ -30,4 +32,25 @@ function makeRequest(url){
 		console.log('connection error')
 	}
 	request.send();
+	console.log("anything")
 }
+function renderPage(results){
+
+		var listItem = "";
+		console.log(results)
+
+		for (var i = 0; i < results.length; i++) {
+			listItem += '<article>';
+			listItem += '<img src="'+ results[i].images +'" alt="" />';
+			listItem += '<h2>' + results[i].name + '</h2>';
+			listItem += '<p>' + results[i].artists.name + '</p>';
+			listItem += '</article>';
+		}
+
+		resultItems.insertAdjacentHTML('beforeend', listItem);
+	
+}
+// results.forEach((result, index, array) => {
+	// 	var listItem = document.createElement('li');
+	// 	listItem.innerHTML = result.name;
+	// 	resultItems.appendChild(listItem);
